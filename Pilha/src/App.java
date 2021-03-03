44e54d94a020d88dcc2eda4d70fba581b9c9b4d8
GiovaneKayser <giovane.kayser16@gmail.com>
2021-03-03 09:02:56 -0300

    adicionado pilha

---
+++ Pilha/src/App.java
@@ -0,0 +1,12 @@
+import service.Pilha;
+;public class App {
+    public static void main(String[] args) throws Exception {
+        Pilha pilha = new Pilha(3);
+        pilha.Adicionar("a");
+        pilha.Adicionar("b");
+        pilha.Adicionar("c");
+        pilha.Remover();
+        pilha.Adicionar("d");
+        pilha.PrintPilha();
+    }
+}
---
+++ Pilha/src/service/Pilha.java
@@ -0,0 +1,39 @@
+package service;
+
+public class Pilha{
+    private String[] pilha;
+    //criar pilha de tamanho x
+    public Pilha(int tamanho) {
+        pilha = new String[tamanho];
+    }
+
+    public String Topo() {
+        return pilha[TopoIndex()];
+    }
+
+    public int TopoIndex() {
+        for (int i = 0; i < pilha.length; i++) {
+            if (pilha[i] == null) {
+                return i;
+            }
+        }
+        return pilha.length -1;
+    }
+
+    public void Adicionar(String item) {
+        pilha[TopoIndex()] = item;
+    }
+
+    public void Remover() {
+        pilha[TopoIndex()] = null;
+    }
+
+    public void PrintPilha() {
+        for (int i = pilha.length - 1; i > -1; i--) {
+            if (pilha[i] != null) {
+                System.out.println(pilha[i]);
+            }
+            
+        }
+    }
+}
